3a8a0d2c45ae9dbc332cbe41c9223781476c12f1
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-12-02 21:37:22 +0530

    Profile Page Header

---
+++ components/ProfileHeader.js
@@ -0,0 +1,80 @@
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import { auth } from "../firebase";
+import { UserCircleIcon } from "react-native-heroicons/solid";
+
+export default function ProfileHeader() {
+  const user = auth.currentUser;
+  const userName = user.email.split("@gmail.com");
+
+  const buttonsData = [
+    {
+      id: 1,
+      title: "Your Orders",
+    },
+    {
+      id: 2,
+      title: "Buy Again",
+    },
+    {
+      id: 3,
+      title: "Your Account",
+    },
+    {
+      id: 4,
+      title: "Your Lists",
+    },
+  ];
+
+  return (
+    <View>
+      {/* Profile User Info */}
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          paddingHorizontal: 20,
+          paddingVertical: 10,
+        }}
+      >
+        <Text style={{ fontSize: 22, letterSpacing: 1.1 }}>
+          Hello, <Text style={{ fontWeight: "bold" }}>{userName}</Text>
+        </Text>
+        <UserCircleIcon color={"#000"} size={44} />
+      </View>
+      {/* Buttons */}
+      <View>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            paddingHorizontal: 20,
+            flexWrap: "wrap",
+          }}
+        >
+          {buttonsData.map((data, index) => {
+            return (
+              <TouchableOpacity
+                key={index}
+                style={{
+                  backgroundColor: "#fafafa",
+                  borderWidth: 1,
+                  paddingHorizontal: 14,
+                  paddingVertical: 18,
+                  borderRadius: 50,
+                  width: "48%",
+                  marginTop: 10,
+                }}
+              >
+                <Text style={{ fontSize: 18, textAlign: "center" }}>
+                  {data.title}
+                </Text>
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+      </View>
+    </View>
+  );
+}
